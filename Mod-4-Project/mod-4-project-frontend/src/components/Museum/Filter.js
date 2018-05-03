5790be63d3c65f9d782c69a44b9506daaebbb0a8
shirleyz9402 <shirleyz9402@yahoo.com>
2018-05-03 17:10:25 -0400

    working on the filtersx3

--- Mod-4-Project/mod-4-project-frontend/src/components/Museum/Filter.js
+++ Mod-4-Project/mod-4-project-frontend/src/components/Museum/Filter.js
@@ -4,29 +4,40 @@ export default class Filter extends React.Component{
   constructor(){
     super()
     this.state = {
-      artists:[]
+      artists:[],
+      types: [],
+      option: 'artists'
 
     }
   }
   componentDidMount(){
     fetch('https://www.rijksmuseum.nl/api/en/collection/?key=BDC9BYuC&format=json').then(r => r.json()).then(r => r.facets[0].facets.forEach(n => this.setState({artists: [...this.state.artists, n.key]})))
+    fetch('https://www.rijksmuseum.nl/api/en/collection/?key=BDC9BYuC&format=json').then(r => r.json()).then(r => r.facets[4].facets.forEach(n => this.setState({types: [...this.state.types, n.key]})))
+  }
+  handleChange = event => {
+    this.setState({option: event.target.value})
   }
   render(){
-    console.log(this.state)
     const renderArtistFilter = this.state.artists.map((artist,i) => {
       return(
         <option key = {i} value={artist}>{artist}</option>
       )
     })
+    const renderTypeFilter = this.state.types.map((type, i) => {
+      return(
+        <option key = {i} value={type}>{type}</option>
+      )
+    })
+
     return(
       <div>
         Filter By:
-        <select id="filter-category">
-          <option value="artist">Artists</option>
+        <select onChange = {this.handleChange} id="filter-category">
+          <option value="artists">Artists</option>
           <option value="type">Type</option>
         </select>
         <select id="filter-options">
-          {renderArtistFilter}
+          {this.state.option === 'artists' ? renderArtistFilter : renderTypeFilter}
         </select>
       </div>
     )
