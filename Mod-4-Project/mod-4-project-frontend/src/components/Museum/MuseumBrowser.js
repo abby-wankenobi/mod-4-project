7f60f61e47c90b2f0d188f9a86b4743c1c3b560c
Abigail Swarth <abigailswarth@gmail.com>
2018-05-06 14:29:19 -0400

    stuff

--- Mod-4-Project/mod-4-project-frontend/src/components/Museum/MuseumBrowser.js
+++ Mod-4-Project/mod-4-project-frontend/src/components/Museum/MuseumBrowser.js
@@ -1,19 +1,88 @@
 import React from 'react';
-// import ArtCard from '../containers/ArtCard'
+import ArtCard from '../../containers/ArtCard'
+import ArtistCard from '../../containers/ArtistCard'
 
 export default class MuseumBrowser extends React.Component{
-  render(){
-    const renderArt = this.props.art.map((a,i) => {
-      return(
-        <div key = {i} id="artist-image">
-          <img height="200" width="auto" src= {a.webImage.url}/>
-        </div>
-      )
+
+  state = {
+    art: "allart",
+    galleries: [],
+    selectedGallery: null
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3000/galleries')
+    .then(r => r.json())
+    .then(json => this.setState({
+      galleries: json
+    }))
+  }
+
+  saveToGallery = () => {
+
+    let body = {artwork: {
+      title: this.state.art.title,
+      artist: this.state.art.principalMaker,
+      image: this.state.art.webImage.url,
+      gallery_id: this.state.selectedGallery
+    }}
+
+    fetch('http://localhost:3000/artworks', {
+      method: "POST",
+      headers: {"Content-Type" : "application/json"},
+      body: JSON.stringify(body)
     })
+    .then(r => r.json())
+    .then(console.log)
+  }
+
+  findArtShit = (id) => {
+    return this.props.art.find(art => art.id === id)
+  }
+
+
+  handleClick = (e) => {
+    this.setState({
+      art: this.findArtShit(e.target.id)
+    })
+  }
+
+  handleArtistClick = () => {
+    this.setState({
+      art: "allart"
+    })
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      selectedGallery: e.target.value
+    })
+  }
+
+  render(){
+
+    var renderArt = ""
+
+    if (this.state.art === "allart") {
+      renderArt = this.props.art.map((art) => {
+        return <ArtCard art={art} handleClick={this.handleClick}/>
+      })
+    } else {
+      renderArt = <ArtistCard selectedGallery={this.state.selectedGallery}
+                              galleries={this.state.galleries}
+                              art={this.state.art}
+                              handleArtistClick={this.handleArtistClick}
+                              handleChange={this.handleChange}
+                              saveToGallery={this.saveToGallery}/>
+    }
+
+
+
+console.log(this.props.art)
 
     return(
       <div className="grid-container">
-        {renderArt}
+       {renderArt}
       </div>
     )
   }
---
+++ Mod-4-Project/mod-4-project-frontend/src/containers/ArtistCard.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const ArtistCard = props => {
+
+  const {art} = props;
+
+  let galleryOption = props.galleries.map(gallery => {
+    return <option value={gallery.id}>{gallery.name}</option>
+  })
+
+  return(
+    <div className="artistdiv">
+      <img height="550" width="auto" src= {art.webImage.url}/>
+      <div id="artist-info">
+        <h1>{art.title}</h1>
+        <h3>{art.principalMaker}</h3>
+        <p>Date: {art.dating.presentingDate}</p>
+        <p>Materials: {art.materials.map(art => art).join(" ")}</p>
+        <p>Add to Gallery</p>
+        <select onChange={props.handleChange}>
+          <option>Select Gallery</option>
+          {galleryOption}
+        </select>
+        <button onClick={props.saveToGallery}>Save</button>
+        <br></br>
+        <br></br>
+        <br></br>
+        <button onClick={props.handleArtistClick}>Back to Artist Page</button>
+      </div>
+    </div>
+  )
+
+
+}
+
+export default ArtistCard
