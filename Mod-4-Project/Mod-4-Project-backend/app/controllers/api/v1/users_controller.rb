6462c57791b552e67e961cf2bedbda1dcd9cfed1
shirleyz9402 <shirleyz9402@yahoo.com>
2018-05-07 09:07:25 -0400

    sessions

--- Mod-4-Project/Mod-4-Project-backend/app/controllers/api/v1/users_controller.rb
+++ Mod-4-Project/Mod-4-Project-backend/app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,15 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :require_logged_in, only: [:home, :death]
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+    return redirect_to controller: 'users', action: 'new' unless @user.save
+    session[:user_id] = @user.id
+    redirect_to controller: 'users', action: 'home'
+  end
+
   def index
     @users = User.all
     render json: @users
